38b11d110ad7a261e831f4610faa7e09d5ff2c40
Agustin Quiceno Z <san.agustin20.aqz@gmail.com>
2021-12-17 21:12:05 -0500

    ejercicios del 5 al 8

--- js/modulos.js
+++ js/modulos.js
@@ -82,26 +82,81 @@ multiplicarStr("Hola Agus");
 
 /*5) Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
 ---------------mi solucion----------------------------------------------*/
+const invertirStr = (str = "") => 
+    (!str)
+        ?console.warn('No se ha introducido ningun texto')
+        :console.info(str.split("").reverse().join(""));
 
+invertirStr("hola ema de nuevo")
 /*-----------solucion optima--------------------------------------------*/
-
+//la misma que la mia
 
 
 /*6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
 ---------------mi solucion----------------------------------------------*/
-
+// no la pude hacer
 /*-----------solucion optima--------------------------------------------*/
+const contarRepetido = (str="", clave="") => {
+    if(!str)
+        return console.warn("No se ha ingresado texto");
+    if(!clave)
+        return console.warn("No se ha ingresado la palabra clave");
+    
+    let i=0, contador=0;
 
-
+    while(i !== -1){
+        i = str.indexOf(clave, i);
+        if(i !== -1) {
+            i++;
+            contador ++;
+        }
+    }
+    return console.info(`La palabra ${clave} se repite ${contador} veces.`)
+}
+//invocar la funcion
+contarRepetido("Hola hola hola, como estamos je je je", "je");
 
 /*7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
----------------mi solucion----------------------------------------------*/
-
-/*-----------solucion optima--------------------------------------------*/
-
+---------------mi solucion----------------------------------------------
+const palindroma = (str = "") => {
+    if(!str)
+        return console.warn("No se ha ingresado ninguna palabra");
+    str = str.toLowerCase();
+    let invertida = str.split("").reverse().join("");
+    if(str === invertida)
+        return console.info(`La palabra "${str}" es palindroma`);
+    else
+        return console.info(`La palabra "${str}" no es palindroma`)
+}
+palindroma("Salas");
+-----------solucion optima--------------------------------------------*/
+const palindroma = (str = "") => {
+    if(!str)
+        return console.warn("No se ha ingresado ninguna palabra");
+    str = str.toLowerCase();
+    let invertida = str.split("").reverse().join("");
+    return(str === invertida)
+        ?console.info(`La palabra "${str}" es palindroma`)
+        :console.info(`La palabra "${str}" no es palindroma`)
+}
+palindroma("Salas");
 
 /*8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
 ---------------mi solucion----------------------------------------------*/
 
 /*-----------solucion optima--------------------------------------------*/
+const eliminarCaracteres = (str="", clave="") =>
+    (!str)
+        ?console.warn("No se ha ingresado texto")
+        :(!clave)
+            ?console.warn("No se ha ingresado palabra clave")
+            :console.info(str.replace(new RegExp(clave, "ig"), ""));
 
+eliminarCaracteres("a1, b2, c3, d4, a5", "a");
+/*9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
+
+
+10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
+
+
+11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120. */
